a5297ca02d452512031acf8b29ebb412daa8660a
Fillipe <fillipe484@gmail.com>
2020-08-03 18:18:49 -0300

    Create teacher card
    create and style a responsive teacher info card in TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css'
 
 function TeacherList(){
@@ -25,6 +27,36 @@ function TeacherList(){
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
+            <div>
+              <strong>Diego Fernandes</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada
+            <br /><br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas atraves de experiências. Mais de 2000 pessoas ja passaram pelas minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00 </strong>
+            </p>
+            <button type="button" >
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
+
     </div>
   )
 }
