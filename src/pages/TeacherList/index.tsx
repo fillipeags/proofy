8bf9649790dae254aad9a91832711f20738bd4c7
Fillipe <fillipe484@gmail.com>
2020-08-03 18:32:11 -0300

    Create Teacher Info Component

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css'
 
@@ -29,32 +28,11 @@ function TeacherList(){
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes"/>
-            <div>
-              <strong>Diego Fernandes</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de química avançada
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas atraves de experiências. Mais de 2000 pessoas ja passaram pelas minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00 </strong>
-            </p>
-            <button type="button" >
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
       </main>
 
     </div>
